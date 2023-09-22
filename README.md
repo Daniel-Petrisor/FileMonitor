190047db61f8e820b24b35eb530ab51e8dc802a9
AsusZenBook <danielpetrisor@live.it>
2023-09-22 18:55:24 +0200

    mostra il contenuto del file ogni volta che viene modificato

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # FileMonitor
 ## Monitorare le modifiche a un file di testo in una rete aziendale.
 
+
+Utilizzare la libreria watchdog per monitorare più file o anche intere directory
+Watchdog fornisce un PollingObserver che controlla attivamente i file per le modifiche invece di fare affidamento sugli eventi del file system.
+Questo può essere più affidabile per i file di rete, ma potrebbe anche essere più lento o utilizzare più risorse del sistema rispetto al metodo standard.
+Se vuoi monitorare più file, puoi semplicemente chiamare il metodo schedule più volte, una volta per ogni file che vuoi monitorare.
+Se vuoi monitorare un’intera directory e tutti i suoi sottodirectory, 
+puoi passare il percorso della directory al metodo schedule e impostare il parametro recursive su True.
+
+
+
+
 Questo script stampa un messaggio ogni volta che il file specificato viene modificato.
 
 1. Installare la libreria watchdog
--- monitor.py
+++ monitor.py
@@ -1,16 +1,44 @@
 from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
+import os
 import time
+from datetime import datetime
+
+
+
+file_path = 'D:/Github/FileMonitor/Current.txt'
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        print(f'Il file {event.src_path} è stato modificato')
+        now = datetime.now()                                    # Ottieni l'ora corrente
+        now_str = now.strftime("%d/%m/%Y, %H:%M:%S")            # Formatta l'ora come stringa
+        print("File modificato: ",now_str, event.src_path)      # Stampiamo il percorso del file che è stato modificato
+
+        # Controlliamo se il percorso modificato è un file
+        if os.path.isfile(event.src_path):
+            try:
+                # Apriamo il file in modalità lettura
+                with open(event.src_path, 'r') as file:        # Leggiamo e stampiamo il contenuto del file
+                    print(file.read())
+
+            except Exception as e:
+                print(f"Si è verificato un errore durante la lettura del file: {e}")
+
+
+
+
 
 if __name__ == "__main__":
     event_handler = MyHandler()
-    observer = Observer()
-    # Se desideri monitorare anche la creazione di nuovi file, dovrai impostare recursive=True
-    observer.schedule(event_handler, path='percorso_del_tuo_file', recursive=False)
+
+    # Creiamo un'istanza dell'Observer di watchdog
+    # observer = Observer() # Metodo standard per monitorare i file system del sistema locale
+    observer = PollingObserver() # Metodo per monitorare i file su una rete locale
+    observer.schedule(event_handler, path=file_path, recursive=False)
+
+    # Se desideri monitorare anche la creazione di nuovi file, oppure un'intera directory, dovrai impostare recursive=True
+    # observer.schedule(event_handler, path='path_to_your_directory', recursive=True)
     observer.start()
 
     try:
